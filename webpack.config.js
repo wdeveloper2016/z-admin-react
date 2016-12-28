cd91b449b41cee5f32edb74a65b0ee5b6a3aba70
wdeveloper2016 <wdeveloper2016@gmail.com>
2016-12-28 08:51:29 +0000

    Basic Structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        excludes: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
