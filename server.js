5463e0997ecbefd7518c66eac8ffe948951f4762
wdeveloper2016 <wdeveloper2016@gmail.com>
2017-01-04 01:46:43 +0000

    Server.js modification

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var http = require("http");
+var url = require("url");
+var consolidate = require('consolidate');
+var handlebars = require('handlebars');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
 
 // Create our app
 var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+var portNumber = process.argv[2] || 3000;
+
+app.listen(portNumber, function () {
+  console.log('Server listening at port '+ portNumber);
+	routes.initialize(app);
 });
